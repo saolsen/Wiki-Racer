cd98d890770c403ca0f13954cbc1f101cd388f73
Stephen Olsen <steveo@hack-net.org>
2011-04-03 17:09:12 -0500

    Finished up the wikiparse script

--- src/setup/wikiparse.py
+++ src/setup/wikiparse.py
@@ -2,10 +2,13 @@
 # This is the script used to parse the wikipedia xml db dump into
 # a redis database.
 
+import sys
 import re
 from BeautifulSoup import BeautifulStoneSoup
 
 __author__ = 'Stephen Olsen'
+
+wikilink = re.compile(r'(\[\[)([-_a-zA-Z0-90-9.()|  ]*?)(\]\])')
 
 def parsePage(page):
     """
@@ -14,11 +17,11 @@ def parsePage(page):
     Returns a string representation of a graph node
     'pagename:link1,link2,link3' 
     """
-    # page is a string of the page xml
+   
     soup = BeautifulStoneSoup(page)
     node = soup.title.contents[0] + '|'
 
-    for link in re.findall(r'(\[\[)([-_a-zA-Z0-90-9.()|  ]*?)(\]\])', soup.text):
+    for link in wikilink.findall(soup.text):
         rename = link[1].find('|')
         if rename >= 0:
             node += link[1][rename+1:] + ','
@@ -34,32 +37,56 @@ def parseRedirect(redirect):
     Returns a string representation of a redirect
     'pagename:real_link'
     """
-    return 'AccessibleComputing:Computer accessibility'
+    soup = BeautifulStoneSoup(redirect)
+    node = soup.title.contents[0] + '|'
+
+    node += wikilink.search(soup.text).group(2)
+
+    return node
  
 def checkType(page):
     """
     Returns true if the page is a real wikipedia page and false if it is
     a redirect.
     """
-    return true
+    return not bool(re.search('<redirect />', page))
 
-def main(document_name):
+def main():
     """
     Takes a xml document and return two files, nodes and redirects
     """
+    try:
+        infile = sys.argv[1]
+        f = open(infile, 'r')
+        n = open('./nodes', 'a')
+        r = open('./redirects' , 'a')
 
-    #Go through the file page by page,
-    #use a simple regex to get everything between the two <data> or whatever
-    #pages.
+        page = []
 
-    #Parse that string. This way I dont need a real xml parser and I get to
-    #learn regex and I get to iterate through the file instead of trying
-    #to read in the whole thing at once.
-
-    # Get the file line by line, each time I have a page, join them and send
-    # the string to the right fuction.
-
-    pass
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            if line.strip() == '<page>':
+                while line.strip() != '</page>':
+                    page.append(line)
+                    line = f.readline()
+            page.append(line)
+            page_str = "".join(page)
+            if checkType(page_str):
+                node = parsePage(page_str)
+                n.write(node + '\n')
+                print 'Node: ', node
+            else:
+                redirect = parseRedirect(page_str)
+                r.write(redirect + '\n')
+                print 'Redirect: ', redirect
+            page = []
+        f.close()
+        n.close()
+        r.close()
+    except IndexError:
+        print "Please specify an input file"
 
 if __name__=="__main__":
     main()
--- src/setup/wikiparseTest.py
+++ src/setup/wikiparseTest.py
@@ -5,14 +5,17 @@
 import wikiparse # Parses the db dump into a flat file of nodes
 import unittest2
 
-class parsePageTestCase(unittest2.TestCase):
+class wikiparseTestCase(unittest2.TestCase):
     def setUp(self):
         f = open('./testdata/anarchism.page', 'r')
         self.testRealPage = "".join(f.readlines())
         f.close()
         f = open('./testdata/testpage.page', 'r')
         self.testFakePage = "".join(f.readlines())
-        f.close()    
+        f.close()
+        f = open('./testdata/accessablecomputing.redirect', 'r')
+        self.testRedirect = "".join(f.readlines())
+        f.close() 
 
     def testReal(self):
         pass
@@ -22,22 +25,16 @@ class parsePageTestCase(unittest2.TestCase):
         result = 'Test Page|link one,link two,there,how about another link'
         self.assertEqual(self.fakeNode, result)
 
-class parseRedirectTestCase(unittest2.TestCase):
-    def setUp(self):
-        f = open('./testdata/accessablecomputing.redirect', 'r')
-        self.testRedirect = f.readlines()
-        f.close()
- 
     def testRedirect(self):
         self.redirect = wikiparse.parseRedirect(self.testRedirect)
-        result = 'AccessibleComputing:Computer accessibility'
+        result = 'AccessibleComputing|Computer accessibility'
         self.assertEqual(self.redirect, result)
 
-class checkTypeTestCase(unittest2.TestCase):
-    pass
-
-class mainTestCase(unittest2.TestCase):
-    pass
+    def testCheckType(self):
+        self.isRedirect = wikiparse.checkType(self.testRedirect)
+        self.notRedirect = wikiparse.checkType(self.testFakePage)
+        self.assertEqual(self.isRedirect, False)
+        self.assertEqual(self.notRedirect, True)
 
 if __name__ == '__main__':
     unittest2.main()
